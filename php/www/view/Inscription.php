bcf8fddfca64aeb801707c7f865ec6223deeec4f
ossoua <48774099+ossoua@users.noreply.github.com>
2019-11-21 15:53:32 +0100

    maj tests et profil + footer

--- php/www/view/Inscription.php
+++ php/www/view/Inscription.php
@@ -80,8 +80,9 @@
     </form>
   </section>
   <footer>
+    <a href="GCU.html"> Conditions générales d'utilisation & Règles de confidentialités </a>
+    Navajo 2019(©)
   </footer>
-
 </body>
 
 </html>
