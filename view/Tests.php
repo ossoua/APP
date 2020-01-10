7ff38bb16fd05b906a95a8b695bbd5e88c651a1e
lia-th <56994992+lia-th@users.noreply.github.com>
2020-01-10 11:52:38 +0100

    maj synthaxe

--- view/Tests.php
+++ view/Tests.php
@@ -19,6 +19,7 @@
     <div id="searchbar">
       <input type="search" placeholder="Rechercher sur le site..."/>
     </div>
+    
     <div class="help">
       <a href="FAQ.html"><img src="/view/img/help.png" alt="Help" width="25px" title="Besoin d'aide ? FAQ"></a>
     </div>
