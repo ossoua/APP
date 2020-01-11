00924894af5d88ac397b7fd9dee920f4fe4447c0
ossoua <48774099+ossoua@users.noreply.github.com>
2020-01-11 18:47:09 +0100

    header page admin

---
+++ view/header_admin.php
@@ -0,0 +1,20 @@
+<header>
+  <div id="logo">
+    <img src="/view/img/logo_produit.png" alt="Logo Produit" width="100px">
+    <img class="padding_menu" src="/view/img/DrivingAces.png" alt="DrivingAces" width="230px">
+  </div>
+
+
+  <div class="help">
+    <a href="FAQ.html"><img src="/view/img/help.png" alt="Help" width="25px" title="Besoin d'aide ? FAQ"></a>
+  </div>
+
+  <div class="profil_admin">
+      <strong>Espace Admin</strong><button class="profil_btn"><img src="/view/img/account_admin.png" width="35px"></button>
+      <div class="profil_content">
+        <a href="#section1">Mon espace</a>
+        <a href="#">Link 2</a>
+      </div>
+  </div>
+
+</header>
