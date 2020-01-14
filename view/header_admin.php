7c477026da969ffe54c44c38dc4e7e97f7a37121
ossoua <48774099+ossoua@users.noreply.github.com>
2020-01-14 15:19:26 +0100

    update header

--- view/header_admin.php
+++ view/header_admin.php
@@ -9,12 +9,14 @@
     <a href="/FAQ"><img src="/view/img/help.png" alt="Help" width="25px" title="Besoin d'aide ? FAQ"></a>
   </div>
 
-  <div class="profil_admin">
+  <div id="profil">
+
+    <div class="profil_admin">
       <strong>Admin</strong><button class="profil_btn"><img src="/view/img/account_admin.png" width="35px"></button>
       <div class="profil_content">
         <a href="/backoffice"><img src="/view/img/admin.png" alt="Help" width="20px">Mon espace</a>
         <a href="/user/logout"><img src="/view/img/deco_admin.png" width="20px">Déconnexion</a>
-      </div>
+    </div>
   </div>
 
 </header>
