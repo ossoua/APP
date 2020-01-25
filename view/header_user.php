8a7686dfe1aa8e627b226cf70d65190b33ee1cce
ossoua <48774099+ossoua@users.noreply.github.com>
2020-01-25 18:48:10 +0100

    suppr service client

--- view/header_user.php
+++ view/header_user.php
@@ -15,7 +15,6 @@
         <div class="profil_content">
           <a href="/user/profile"><img src="/view/img/person.png" width="20px">Mon compte</a>
           <a href="/user/tests"><img src="/view/img/tests.png" width="20px">Mes Tests</a>
-          <!-- <a href="Service_client.html"><img src="/view/img/service_client.png" width="20px">Service Client</a>-->
           <a href="/administration"><img src="/view/img/message.png" width="20px">Devenir Administrateur</a>
           <a href="/user/logout"><img src="/view/img/deco_user.png" width="20px">Déconnexion</a>
         </div>
