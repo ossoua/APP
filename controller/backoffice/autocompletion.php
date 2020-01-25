3359ed3e1a28613bae0a0268390469cf873674fc
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-25 09:21:46 +0100

    Revert "Merge branch 'master' of https://github.com/ossoua/APP"
    This reverts commit da4e6413d9050db4dead75cf98be3b8311f623f2, reversing
    changes made to 2b1aed7e9475e57af2034fd3a0bd7d1fb45cc91e.

--- controller/backoffice/autocompletion.php
+++ controller/backoffice/autocompletion.php
@@ -1,33 +1,21 @@
 <?php
 
-//A MODIFIER EN LIGNE
-require_once '/Users/theomartinez/Cours/APP/WEB/www/model/UserManager.php';
+require_once '/Users/theomartinez/Cours/APP/WEB/www/model/BackofficeManager.php';
 
 
 
-$back_office = new UserManager();
+$back_office = new BackofficeManager();
 $users = $back_office->getUsers();
 
 $usersLength = count($users);
 
 $selected_users = [];
 
-if (isset($_GET['search'])) {
-    for ($i = 0; $i < $usersLength; $i++) {
-        if (stripos($users[$i], strval($_GET['search'])) === 0) {
-            array_push($selected_users, $users[$i]);
-        }
+
+for ($i = 0;$i<$usersLength;$i++){
+    if (stripos($users[$i],strval($_GET['search'])) === 0){
+        array_push($selected_users,$users[$i]);
     }
-    echo implode('|', $selected_users);
 }
 
-if (isset($_GET['access_code'])) {
-    if ($_GET['remove'] == "true") {
-        $back_office->removeUser($_GET['access_code']);
-        header('Location: /backoffice');
-    } else {
-    $user = $back_office->getUserInfo($_GET['access_code']);
-    echo implode('|', $user);
-    }
-    header('Location: /backoffice');
-}
+echo implode('|',$selected_users);
--- model/UserManager.php
+++ model/UserManager.php
@@ -1,138 +1,30 @@
 <?php
 
-require_once '/Users/theomartinez/Cours/APP/WEB/www/model/Manager.php';
+require_once 'model/Manager.php';
 
 
 class UserManager extends Manager
 {
 
-    private $db;
-    private $data;
-    private $users = array();
+    protected $db;
 
     public function __construct()
     {
         $this->db = $this->dbConnect();
     }
 
-    public function createUser(){
-        $req = $this->db->prepare('SELECT access_code FROM user');
-        $req->execute();
-        $array = [];
-
-        foreach ($req as $code){
-            array_push($array,$code['access_code']);
-        }
-
-        $max = max($array);
-        $access_code = $max + 1;
-
-
-        $new = $this->db->prepare("INSERT INTO user (id_user, access_code, password, name, first_name, adress, mail, phone, admin) VALUES (NULL, :access_code, '', '', '', '', '', '', '0')");
-        $new->execute(array(
-           'access_code' => $access_code
-        ));
-
-        setcookie('newcode',$access_code);
-        echo 'Nouveau code généré';
-    }
-
-    public function setUser($name, $first_name, $adress, $mail, $access_code, $password)
+    public function setUser($name,$first_name,$adress,$mail,$access_code,$password)
     {
         $req = $this->db->prepare('UPDATE user SET password = :password, name = :name, first_name = :first_name, adress = :adress, mail = :mail WHERE access_code = :access_code');
         $req->execute(array(
-            'password' => $password,
-            'name' => $name,
-            'first_name' => $first_name,
-            'adress' => $adress,
-            'mail' => $mail,
-            'access_code' => $access_code
-        ));
+      'password' => $password,
+      'name'  => $name,
+      'first_name' => $first_name,
+      'adress' => $adress,
+      'mail' => $mail,
+      'access_code' => $access_code
+    ));
 
         echo "Inscription validée";
     }
-
-    function getUsers()
-    {
-        $req = $this->db->prepare('SELECT name, first_name, access_code FROM user');
-        $req->execute();
-        $this->data = $req;
-
-        $i=0;
-        foreach ($this->data as $row){
-            $this->users[$i] = $row['first_name'] . " " . $row['name'] . " <br> Code d'accès: " . $row['access_code'];
-            $i++;
-        }
-
-        return $this->users;
-    }
-
-    function getUserInfo($access_code){
-        $req = $this->db->prepare('SELECT name, first_name, adress, mail, admin FROM user WHERE access_code = :access_code');
-        $req->execute(array(
-            'access_code' => $access_code
-        ));
-        $req = $req->fetch();
-        return [$req['first_name'],$req['name'],$req['adress'],$req['mail'],$req['admin'],$access_code];
-    }
-
-    public function editUserName($name,$access_code)
-    {
-        $req = $this->db->prepare('UPDATE user SET name = :name WHERE access_code = :access_code');
-        $req->execute(array(
-            'name' => $name,
-            'access_code' => $access_code
-        ));
-    }
-
-    public function editUserFirstName($first_name,$access_code)
-    {
-        $req = $this->db->prepare('UPDATE user SET first_name = :first_name WHERE access_code = :access_code');
-        $req->execute(array(
-            'first_name' => $first_name,
-            'access_code' => $access_code
-        ));
-    }
-
-    public function editUserAdress($adress,$access_code)
-    {
-        $req = $this->db->prepare('UPDATE user SET adress = :adress WHERE access_code = :access_code');
-        $req->execute(array(
-            'adress' => $adress,
-            'access_code' => $access_code
-        ));
-    }
-
-    public function editUserMail($mail,$access_code)
-    {
-        $req = $this->db->prepare('UPDATE user SET mail = :mail WHERE access_code = :access_code');
-        $req->execute(array(
-            'mail' => $mail,
-            'access_code' => $access_code
-        ));
-    }
-
-    public function editUserPassword($password,$access_code)
-    {
-        $req = $this->db->prepare('UPDATE user SET password = :password WHERE access_code = :access_code');
-        $req->execute(array(
-            'password' => $password,
-            'access_code' => $access_code
-        ));
-    }
-
-    public function getFreeCodes(){
-        $req = $this->db->prepare("SELECT access_code FROM user WHERE name = '' ");
-        $req->execute();
-        return $req;
-    }
-
-    public function removeUser($access_code){
-        $req = $this->db->prepare('DELETE FROM user WHERE access_code = :access_code');
-        $req->execute(array(
-            'access_code' => $access_code
-        ));
-    }
 }
-
-
