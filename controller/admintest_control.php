36e07e4e1dd1e99e277102949fc5354cffc67f7b
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-26 17:15:26 +0100

    Update admintest_control.php

--- controller/admintest_control.php
+++ controller/admintest_control.php
@@ -2,31 +2,33 @@
 
 session_start();
 
+$message = "";
+
 if (isset($_POST['tonalité'])) {
   $cookie_tonalite = $_POST['tonalité'];
-  setcookie("tonalité", $cookie_tonalite, time() + (864 * 30), "/");
+  setcookie("tonalité", $cookie_tonalite, time() + (20), "/");
 }
 
 if (isset($_POST['son'])) {
   $cookie_son = $_POST['son'];
-  setcookie("son", $cookie_son, time() + (864 * 30), "/");
+  setcookie("son", $cookie_son, time() + (20), "/");
 }
 
 if (isset($_POST['vision'])) {
   $cookie_vision = $_POST['vision'];
-  setcookie("vision", $cookie_vision, time() + (864 * 30), "/");
+  setcookie("vision", $cookie_vision, time() + (20), "/");
 }
 
 if(isset($_COOKIE["tonalité"])) {
-  $tonalite = "Test de reconnaissance de tonalité";
+  $message = "Test de reconnaissance de tonalité";
 }
 
 if(isset($_COOKIE["son"])) {
-  $son = "Test de réaction à un signal sonore innattendu";
+  $message = "Test de réaction à un signal sonore innattendu";
 }
 
 if(isset($_COOKIE["vision"])) {
-  $vision = "Test de réaction à un signal visuel attendu";
+  $message = "Test de réaction à un signal visuel attendu";
 }
 
 require $_SERVER['DOCUMENT_ROOT'].'/view/AdminTest.php';
