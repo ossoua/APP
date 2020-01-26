8ce46e0f45343d1aa10cc864d0f1bc5dbdc85af5
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-26 16:07:27 +0100

    Update admintest_control.php

--- controller/admintest_control.php
+++ controller/admintest_control.php
@@ -2,21 +2,30 @@
 
 session_start();
 
-$cookie_tonalite = $_POST['tonalité'];
-$cookie_son = $_POST['son'];
-$cokie_vision = $_POST['vision'];
+if (isset($_POST['tonalité'])) {
+  $cookie_tonalite = $_POST['tonalité'];
+  setcookie("tonalité", $cookie_tonalite, time() + (864 * 30), "/");
+}
 
-setcookie($cookie_tonalité, $cookie_son, $cokie_vision, time() + (86400 * 30), "/"); // 86400 = 1 jour
+if (isset($_POST['son'])) {
+  $cookie_son = $_POST['son'];
+  setcookie("son", $cookie_son, time() + (864 * 30), "/");
+}
 
-if(isset($_COOKIE[$cookie_tonalite])) {
+if (isset($_POST['vision'])) {
+  $cookie_vision = $_POST['vision'];
+  setcookie("vision", $cookie_vision, time() + (864 * 30), "/");
+}
+
+if(isset($_COOKIE["tonalité"])) {
   $tonalite = "Test de reconnaissance de tonalité";
 }
 
-if(isset($_COOKIE[$cookie_son])) {
+if(isset($_COOKIE["son"])) {
   $son = "Test de réaction à un signal sonore innattendu";
 }
 
-if(isset($_COOKIE[$cookie_vision])) {
+if(isset($_COOKIE["vision"])) {
   $vision = "Test de réaction à un signal visuel attendu";
 }
 
