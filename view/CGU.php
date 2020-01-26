ed1ad2ad77849c308529d0e012162b9449ef44c2
ossoua <48774099+ossoua@users.noreply.github.com>
2020-01-26 12:22:33 +0100

    création page CGU

---
+++ view/CGU.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Conditions générales d'utilisation</title>
+    <link rel="stylesheet" href="/view/stylesheets/style_header.css">
+
+  </head>
+  <body>
+  <?php require './view/header_user.php'?>
+
+  </body>
+</html>
--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,4 @@
 <footer>
-  <a href="GCU.html"> Conditions générales d'utilisation & Règles de confidentialités </a>
+  <a href="GCU.php"> Conditions générales d'utilisation & Règles de confidentialités </a>
   Navajo 2019(©)
 </footer>
